1cd4c26d799e3bea5bdb2fa555790b1f7d0bffa9
Mahdi <mahdimahdi9731@gmail.com>
2026-06-02 18:30:22 +0330

    Initializing project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 500,
+        fontSize: 22,
+        marginLeft: 600,
+      }}
+    >
+      <p>Current Version:</p>
+      <p style={{ marginLeft: 10, fontWeight: "bold" }}>7</p>
+    </div>
+  );
+}
+
+export default App;
