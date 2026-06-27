eb81ff744563b0436ab6a7bfd40afb89dbe52d17
Mahdi <mahdimahdi9731@gmail.com>
2026-06-27 11:40:39 +0330

    Redeploy: 31

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
         <div style={{ display: "flex", marginTop: 20 }}>
           <p>Current Version:</p>
-          <p style={{ marginLeft: 10, fontWeight: "bold" }}>30</p>
+          <p style={{ marginLeft: 10, fontWeight: "bold" }}>31</p>
         </div>
       </div>
     </BrowserRouter>
