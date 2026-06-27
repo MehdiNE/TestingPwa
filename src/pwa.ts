7a0173539ed3d44d8010f3184286db6f79da7f25
Mahdi <mahdimahdi9731@gmail.com>
2026-06-27 11:33:41 +0330

    Redeploy: 27

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,28 +1,28 @@
-// import { registerSW } from "virtual:pwa-register";
+import { registerSW } from "virtual:pwa-register";
 
-// const intervalMS = 1 * 60 * 1000;
+const intervalMS = 1 * 60 * 1000;
 
-// export const updateSW = registerSW({
-//   onRegisteredSW(swUrl, r) {
-//     if (!r) return;
+export const updateSW = registerSW({
+  onRegisteredSW(swUrl, r) {
+    if (!r) return;
 
-//     setInterval(async () => {
-//       if (r.installing || !navigator) return;
+    setInterval(async () => {
+      if (r.installing || !navigator) return;
 
-//       if ("connection" in navigator && !navigator.onLine) return;
+      if ("connection" in navigator && !navigator.onLine) return;
 
-//       const resp = await fetch(swUrl, {
-//         cache: "no-store",
-//         headers: {
-//           cache: "no-store",
-//           "cache-control": "no-cache",
-//         },
-//       });
+      const resp = await fetch(swUrl, {
+        cache: "no-store",
+        headers: {
+          cache: "no-store",
+          "cache-control": "no-cache",
+        },
+      });
 
-//       if (resp?.status === 200) await r.update();
-//     }, intervalMS);
-//   },
-// });
+      if (resp?.status === 200) await r.update();
+    }, intervalMS);
+  },
+});
 
 // import { registerSW } from "virtual:pwa-register";
 
