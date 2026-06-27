808ec69250fd8b58b20303ed8097c090e66f708d
Mahdi <mahdimahdi9731@gmail.com>
2026-06-27 11:10:49 +0330

    Redeploy: 20

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       }}
     >
       <p>Current Version:</p>
-      <p style={{ marginLeft: 10, fontWeight: "bold" }}>19</p>
+      <p style={{ marginLeft: 10, fontWeight: "bold" }}>20</p>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: "script",
+      injectRegister: "auto",
 
       pwaAssets: {
         disabled: false,
