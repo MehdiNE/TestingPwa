3584d626172eda90d84ed3ad17837ad2fdcbfc6d
Mahdi <mahdimahdi9731@gmail.com>
2026-06-27 11:12:19 +0330

    Redeploy: 21

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       }}
     >
       <p>Current Version:</p>
-      <p style={{ marginLeft: 10, fontWeight: "bold" }}>20</p>
+      <p style={{ marginLeft: 10, fontWeight: "bold" }}>21</p>
     </div>
   );
 }
